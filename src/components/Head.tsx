1f7f8ad381b0f25a66e263685d99b0ce640f48a8
Chris Leveille <cdleveille@gmail.com>
2022-08-02 00:45:06 -0400

    small tag fix

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,8 +8,8 @@ const Head: React.FC<IHeadProps> = ({ title }) => {
 	return (
 		<NextHead>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<meta name="description" content="Chris Leveille - Software Engineer"></meta>
-			<meta name="theme-color" content="#0d1117"></meta>
+			<meta name="description" content="Chris Leveille - Software Engineer" />
+			<meta name="theme-color" content="#0d1117" />
 			<link rel="apple-touch-icon" sizes="180x180" href="img/icons/icon-apple-touch.png" />
 			<link rel="icon" type="image/png" sizes="32x32" href="img/icons/icon-32x32.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="img/icons/icon-16x16.png" />
