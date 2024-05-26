6f8d9505a9fd00960020325a0ab2e2631827008d
Chris Leveille <cdleveille@gmail.com>
2024-05-26 03:32:18 -0400

    added more site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,27 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Chris Leveille - Software Engineer"
+	title: "Chris Leveille - Software Engineer",
+	description: "Hello! My name is Chris and I am a software engineer from Western Massachusetts.",
+	authors: { name: "Chris Leveille", url: "https://www.cdleveille.net" },
+	openGraph: {
+		title: "Chris Leveille - Software Engineer",
+		description: "Hello! My name is Chris and I am a software engineer from Western Massachusetts.",
+		type: "profile",
+		firstName: "Chris",
+		lastName: "Leveille",
+		username: "cdleveille",
+		emails: "cdleveille@gmail.com",
+		url: "https://www.cdleveille.net",
+		images: {
+			url: "https://avatars.githubusercontent.com/u/1410481",
+			secureUrl: "https://avatars.githubusercontent.com/u/1410481",
+			type: "image/jpeg",
+			alt: "Chris Leveille",
+			width: 460,
+			height: 460
+		}
+	}
 };
 
 const RootLayout = ({
